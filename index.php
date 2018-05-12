b57415257730091b5215019adbf5f3852f50f580
Cynthia <29832823+CynthiaLixinLee@users.noreply.github.com>
2018-05-11 22:05:45 -0400

    Created index and css

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>To Do List</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+  <section class="list">
+    <div class="header">
+      <h1>To Do List</h1>
+    </div>
+    <div class="item">
+      <input type="checkbox">
+      <p>Read You Don't Know JavaScript - Book 3</p>
+    </div>
+    <div class="item">
+      <input type="checkbox">
+      <p>Study algorithms</p>
+    </div>
+    <div class="item">
+      <input type="checkbox">
+      <p>Listen to Code Newbie podcast</p>
+    </div>
+    <div class="item">
+      <input type="checkbox">
+      <p>Check job postings</p>
+    </div>
+    <div class="item">
+      <input type="checkbox">
+      <p>Write Medium article</p>
+    </div>
+    <div class="item">
+      <input type="checkbox">
+      <p>Do research - Jest testing</p>
+    </div>
+  </section>
+
+
+  <?php
+  ?>
+
+</body>
+
+</html>
